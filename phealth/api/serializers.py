b2e302c8f21a5854ff9474973d3429880e953bd2
pk13055 <pkrockstar7@gmail.com>
2018-01-11 05:44:31 +0530

    Rest framework basics added

---
+++ phealth/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+# from rest_framework import <models>
+
+'''
+
+So basically to serialize and send data we have to create corresponding serializers
+for every model in the API
+Models will have to be divided into api and non-api models
+
+'''
+
