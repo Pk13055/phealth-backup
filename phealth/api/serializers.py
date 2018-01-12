b920d6e7f9000f264e5d3b687269a955daf58c6c
pk13055 <pkrockstar7@gmail.com>
2018-01-12 17:41:27 +0530

    Added API Routes
    - Basic DRF generated routes added
    - CRUD for all routes
    - Modify API-endpoints as required

--- phealth/api/serializers.py
+++ phealth/api/serializers.py
@@ -1,16 +1,570 @@
 from rest_framework import serializers
-# from rest_framework import <models>
+from api.models import *
 
-'''
 
-So basically to serialize and send data we have to create corresponding serializers
-for every model in the API
-Models will have to be divided into api and non-api models
-
-'''
-from . models import * 
 
 class AddressSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Address
-		fields = ('__all__') 
+
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class AgegroupSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Agegroup
+        fields = '__all__'
+
+
+class AlertsourcesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Alertsources
+        fields = '__all__'
+
+
+class AlerttypeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Alerttype
+        fields = '__all__'
+
+
+class AppointmentbookingSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Appointmentbooking
+        fields = '__all__'
+
+
+class AvailablefacilitiesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Availablefacilities
+        fields = '__all__'
+
+
+class BookingsourcesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Bookingsources
+        fields = '__all__'
+
+
+class BusinessregtypeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Businessregtype
+        fields = '__all__'
+
+
+class CitySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = City
+        fields = '__all__'
+
+
+class ClinicianMembershipsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = ClinicianMemberships
+        fields = '__all__'
+
+
+class CliniciansSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Clinicians
+        fields = '__all__'
+
+
+class CliniciansDatesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = CliniciansDates
+        fields = '__all__'
+
+
+class CliniciansEducationSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = CliniciansEducation
+        fields = '__all__'
+
+
+class CliniciansExperienceSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = CliniciansExperience
+        fields = '__all__'
+
+
+class CliniciansSpecialitySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = CliniciansSpeciality
+        fields = '__all__'
+
+
+class ConditionsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Conditions
+        fields = '__all__'
+
+
+class CountrySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+
+class CouponacceptSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Couponaccept
+        fields = '__all__'
+
+
+class CouponsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Coupons
+        fields = '__all__'
+
+
+class DesignationSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Designation
+        fields = '__all__'
+
+
+class DiscountcardSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Discountcard
+        fields = '__all__'
+
+
+class DiscountcardFamilySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = DiscountcardFamily
+        fields = '__all__'
+
+
+class DiscountcardFamilyExtraSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = DiscountcardFamilyExtra
+        fields = '__all__'
+
+
+class DistrictSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = District
+        fields = '__all__'
+
+
+class FacilitiesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Facilities
+        fields = '__all__'
+
+
+class FacilitiesServicesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = FacilitiesServices
+        fields = '__all__'
+
+
+class FamilySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Family
+        fields = '__all__'
+
+
+class FeedbackSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Feedback
+        fields = '__all__'
+
+
+class GenderSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Gender
+        fields = '__all__'
+
+
+class HealthcheckbookingSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Healthcheckbooking
+        fields = '__all__'
+
+
+class HealthchecksSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Healthchecks
+        fields = '__all__'
+
+
+class HealthchecksInclusionSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = HealthchecksInclusion
+        fields = '__all__'
+
+
+class HealthprovidersSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Healthproviders
+        fields = '__all__'
+
+
+class HealthprovidersSpecialitySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = HealthprovidersSpeciality
+        fields = '__all__'
+
+
+class HealthrecordsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Healthrecords
+        fields = '__all__'
+
+
+class HealthseekerSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Healthseeker
+        fields = '__all__'
+
+
+class HealthseekerAlertsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = HealthseekerAlerts
+        fields = '__all__'
+
+
+class HealthseekerAvailabilitiesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = HealthseekerAvailabilities
+        fields = '__all__'
+
+
+class HealthseekerHealthrecordsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = HealthseekerHealthrecords
+        fields = '__all__'
+
+
+class HealthseekerTypeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = HealthseekerType
+        fields = '__all__'
+
+
+class InterestSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Interest
+        fields = '__all__'
+
+
+class LanguagesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Languages
+        fields = '__all__'
+
+
+class PhotoalbumSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Photoalbum
+        fields = '__all__'
+
+
+class PlansSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Plans
+        fields = '__all__'
+
+
+class PlansGroupsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = PlansGroups
+        fields = '__all__'
+
+
+class PlansGroupsSpecialitiesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = PlansGroupsSpecialities
+        fields = '__all__'
+
+
+class PriyoritySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Priyority
+        fields = '__all__'
+
+
+class ProfessionSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Profession
+        fields = '__all__'
+
+
+class QualificationSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Qualification
+        fields = '__all__'
+
+
+class ReferrelsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Referrels
+        fields = '__all__'
+
+
+class ReferrelschemeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Referrelscheme
+        fields = '__all__'
+
+
+class RegistrationWelcomeMessageSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = RegistrationWelcomeMessage
+        fields = '__all__'
+
+
+class ResellerpackagesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Resellerpackages
+        fields = '__all__'
+
+
+class ResellersSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Resellers
+        fields = '__all__'
+
+
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Role
+        fields = '__all__'
+
+
+class SalesagentpackagesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Salesagentpackages
+        fields = '__all__'
+
+
+class SalesagentsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Salesagents
+        fields = '__all__'
+
+
+class SalestargetSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Salestarget
+        fields = '__all__'
+
+
+class SecretquestionsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Secretquestions
+        fields = '__all__'
+
+
+class SpecialitySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Speciality
+        fields = '__all__'
+
+
+class SpecialityTypeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = SpecialityType
+        fields = '__all__'
+
+
+class SponsorsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Sponsors
+        fields = '__all__'
+
+
+class SponsorsGroupSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = SponsorsGroup
+        fields = '__all__'
+
+
+class StateSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = State
+        fields = '__all__'
+
+
+class SymptomsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Symptoms
+        fields = '__all__'
+
+
+class TelephoneSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Telephone
+        fields = '__all__'
+
+
+class TestSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Test
+        fields = '__all__'
+
+
+class TestCategorySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = TestCategory
+        fields = '__all__'
+
+
+class TestSubcategorySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = TestSubcategory
+        fields = '__all__'
+
+
+class TestimonialsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Testimonials
+        fields = '__all__'
+
+
+class TimeslotsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Timeslots
+        fields = '__all__'
+
+
+class TrxnAppointmentbookingsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = TrxnAppointmentbookings
+        fields = '__all__'
+
+
+class TrxnCouponsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = TrxnCoupons
+        fields = '__all__'
+
+
+class TrxnDiscountcardSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = TrxnDiscountcard
+        fields = '__all__'
+
+
+class TrxnHealthcheckbookingSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = TrxnHealthcheckbooking
+        fields = '__all__'
+
+
+class TrxnPaymentsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = TrxnPayments
+        fields = '__all__'
+
+
+class UsersSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Users
+        fields = '__all__'
+
+
+class WeekSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Week
+        fields = '__all__'
+
+
+class WellnessplanSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Wellnessplan
+        fields = '__all__'
+
+
+class WellnessplanProfessionSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = WellnessplanProfession
+        fields = '__all__'
+
+
+class WellnessplanSocialhistorySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = WellnessplanSocialhistory
+        fields = '__all__'
--- phealth/api/views.py
+++ phealth/api/views.py
@@ -1,12 +1,408 @@
-from django.shortcuts import render
-from . serializers import *
-from rest_framework import viewsets
-
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from api.serializers import *
+from api.models import *
 
 
-class AddressViewSet(viewsets.ModelViewSet):
-	queryset = Address.objects.all()
-	serializer_class = AddressSerializer
-	
+class AddressViewSet(ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
 
+
+class AgegroupViewSet(ModelViewSet):
+    queryset = Agegroup.objects.all()
+    serializer_class = AgegroupSerializer
+
+
+class AlertsourcesViewSet(ModelViewSet):
+    queryset = Alertsources.objects.all()
+    serializer_class = AlertsourcesSerializer
+
+
+class AlerttypeViewSet(ModelViewSet):
+    queryset = Alerttype.objects.all()
+    serializer_class = AlerttypeSerializer
+
+
+class AppointmentbookingViewSet(ModelViewSet):
+    queryset = Appointmentbooking.objects.all()
+    serializer_class = AppointmentbookingSerializer
+
+
+class AvailablefacilitiesViewSet(ModelViewSet):
+    queryset = Availablefacilities.objects.all()
+    serializer_class = AvailablefacilitiesSerializer
+
+
+class BookingsourcesViewSet(ModelViewSet):
+    queryset = Bookingsources.objects.all()
+    serializer_class = BookingsourcesSerializer
+
+
+class BusinessregtypeViewSet(ModelViewSet):
+    queryset = Businessregtype.objects.all()
+    serializer_class = BusinessregtypeSerializer
+
+
+class CityViewSet(ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class ClinicianMembershipsViewSet(ModelViewSet):
+    queryset = ClinicianMemberships.objects.all()
+    serializer_class = ClinicianMembershipsSerializer
+
+
+class CliniciansViewSet(ModelViewSet):
+    queryset = Clinicians.objects.all()
+    serializer_class = CliniciansSerializer
+
+
+class CliniciansDatesViewSet(ModelViewSet):
+    queryset = CliniciansDates.objects.all()
+    serializer_class = CliniciansDatesSerializer
+
+
+class CliniciansEducationViewSet(ModelViewSet):
+    queryset = CliniciansEducation.objects.all()
+    serializer_class = CliniciansEducationSerializer
+
+
+class CliniciansExperienceViewSet(ModelViewSet):
+    queryset = CliniciansExperience.objects.all()
+    serializer_class = CliniciansExperienceSerializer
+
+
+class CliniciansSpecialityViewSet(ModelViewSet):
+    queryset = CliniciansSpeciality.objects.all()
+    serializer_class = CliniciansSpecialitySerializer
+
+
+class ConditionsViewSet(ModelViewSet):
+    queryset = Conditions.objects.all()
+    serializer_class = ConditionsSerializer
+
+
+class CountryViewSet(ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class CouponacceptViewSet(ModelViewSet):
+    queryset = Couponaccept.objects.all()
+    serializer_class = CouponacceptSerializer
+
+
+class CouponsViewSet(ModelViewSet):
+    queryset = Coupons.objects.all()
+    serializer_class = CouponsSerializer
+
+
+class DesignationViewSet(ModelViewSet):
+    queryset = Designation.objects.all()
+    serializer_class = DesignationSerializer
+
+
+class DiscountcardViewSet(ModelViewSet):
+    queryset = Discountcard.objects.all()
+    serializer_class = DiscountcardSerializer
+
+
+class DiscountcardFamilyViewSet(ModelViewSet):
+    queryset = DiscountcardFamily.objects.all()
+    serializer_class = DiscountcardFamilySerializer
+
+
+class DiscountcardFamilyExtraViewSet(ModelViewSet):
+    queryset = DiscountcardFamilyExtra.objects.all()
+    serializer_class = DiscountcardFamilyExtraSerializer
+
+
+class DistrictViewSet(ModelViewSet):
+    queryset = District.objects.all()
+    serializer_class = DistrictSerializer
+
+
+class FacilitiesViewSet(ModelViewSet):
+    queryset = Facilities.objects.all()
+    serializer_class = FacilitiesSerializer
+
+
+class FacilitiesServicesViewSet(ModelViewSet):
+    queryset = FacilitiesServices.objects.all()
+    serializer_class = FacilitiesServicesSerializer
+
+
+class FamilyViewSet(ModelViewSet):
+    queryset = Family.objects.all()
+    serializer_class = FamilySerializer
+
+
+class FeedbackViewSet(ModelViewSet):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+
+
+class GenderViewSet(ModelViewSet):
+    queryset = Gender.objects.all()
+    serializer_class = GenderSerializer
+
+
+class HealthcheckbookingViewSet(ModelViewSet):
+    queryset = Healthcheckbooking.objects.all()
+    serializer_class = HealthcheckbookingSerializer
+
+
+class HealthchecksViewSet(ModelViewSet):
+    queryset = Healthchecks.objects.all()
+    serializer_class = HealthchecksSerializer
+
+
+class HealthchecksInclusionViewSet(ModelViewSet):
+    queryset = HealthchecksInclusion.objects.all()
+    serializer_class = HealthchecksInclusionSerializer
+
+
+class HealthprovidersViewSet(ModelViewSet):
+    queryset = Healthproviders.objects.all()
+    serializer_class = HealthprovidersSerializer
+
+
+class HealthprovidersSpecialityViewSet(ModelViewSet):
+    queryset = HealthprovidersSpeciality.objects.all()
+    serializer_class = HealthprovidersSpecialitySerializer
+
+
+class HealthrecordsViewSet(ModelViewSet):
+    queryset = Healthrecords.objects.all()
+    serializer_class = HealthrecordsSerializer
+
+
+class HealthseekerViewSet(ModelViewSet):
+    queryset = Healthseeker.objects.all()
+    serializer_class = HealthseekerSerializer
+
+
+class HealthseekerAlertsViewSet(ModelViewSet):
+    queryset = HealthseekerAlerts.objects.all()
+    serializer_class = HealthseekerAlertsSerializer
+
+
+class HealthseekerAvailabilitiesViewSet(ModelViewSet):
+    queryset = HealthseekerAvailabilities.objects.all()
+    serializer_class = HealthseekerAvailabilitiesSerializer
+
+
+class HealthseekerHealthrecordsViewSet(ModelViewSet):
+    queryset = HealthseekerHealthrecords.objects.all()
+    serializer_class = HealthseekerHealthrecordsSerializer
+
+
+class HealthseekerTypeViewSet(ModelViewSet):
+    queryset = HealthseekerType.objects.all()
+    serializer_class = HealthseekerTypeSerializer
+
+
+class InterestViewSet(ModelViewSet):
+    queryset = Interest.objects.all()
+    serializer_class = InterestSerializer
+
+
+class LanguagesViewSet(ModelViewSet):
+    queryset = Languages.objects.all()
+    serializer_class = LanguagesSerializer
+
+
+class PhotoalbumViewSet(ModelViewSet):
+    queryset = Photoalbum.objects.all()
+    serializer_class = PhotoalbumSerializer
+
+
+class PlansViewSet(ModelViewSet):
+    queryset = Plans.objects.all()
+    serializer_class = PlansSerializer
+
+
+class PlansGroupsViewSet(ModelViewSet):
+    queryset = PlansGroups.objects.all()
+    serializer_class = PlansGroupsSerializer
+
+
+class PlansGroupsSpecialitiesViewSet(ModelViewSet):
+    queryset = PlansGroupsSpecialities.objects.all()
+    serializer_class = PlansGroupsSpecialitiesSerializer
+
+
+class PriyorityViewSet(ModelViewSet):
+    queryset = Priyority.objects.all()
+    serializer_class = PriyoritySerializer
+
+
+class ProfessionViewSet(ModelViewSet):
+    queryset = Profession.objects.all()
+    serializer_class = ProfessionSerializer
+
+
+class QualificationViewSet(ModelViewSet):
+    queryset = Qualification.objects.all()
+    serializer_class = QualificationSerializer
+
+
+class ReferrelsViewSet(ModelViewSet):
+    queryset = Referrels.objects.all()
+    serializer_class = ReferrelsSerializer
+
+
+class ReferrelschemeViewSet(ModelViewSet):
+    queryset = Referrelscheme.objects.all()
+    serializer_class = ReferrelschemeSerializer
+
+
+class RegistrationWelcomeMessageViewSet(ModelViewSet):
+    queryset = RegistrationWelcomeMessage.objects.all()
+    serializer_class = RegistrationWelcomeMessageSerializer
+
+
+class ResellerpackagesViewSet(ModelViewSet):
+    queryset = Resellerpackages.objects.all()
+    serializer_class = ResellerpackagesSerializer
+
+
+class ResellersViewSet(ModelViewSet):
+    queryset = Resellers.objects.all()
+    serializer_class = ResellersSerializer
+
+
+class RoleViewSet(ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+
+class SalesagentpackagesViewSet(ModelViewSet):
+    queryset = Salesagentpackages.objects.all()
+    serializer_class = SalesagentpackagesSerializer
+
+
+class SalesagentsViewSet(ModelViewSet):
+    queryset = Salesagents.objects.all()
+    serializer_class = SalesagentsSerializer
+
+
+class SalestargetViewSet(ModelViewSet):
+    queryset = Salestarget.objects.all()
+    serializer_class = SalestargetSerializer
+
+
+class SecretquestionsViewSet(ModelViewSet):
+    queryset = Secretquestions.objects.all()
+    serializer_class = SecretquestionsSerializer
+
+
+class SpecialityViewSet(ModelViewSet):
+    queryset = Speciality.objects.all()
+    serializer_class = SpecialitySerializer
+
+
+class SpecialityTypeViewSet(ModelViewSet):
+    queryset = SpecialityType.objects.all()
+    serializer_class = SpecialityTypeSerializer
+
+
+class SponsorsViewSet(ModelViewSet):
+    queryset = Sponsors.objects.all()
+    serializer_class = SponsorsSerializer
+
+
+class SponsorsGroupViewSet(ModelViewSet):
+    queryset = SponsorsGroup.objects.all()
+    serializer_class = SponsorsGroupSerializer
+
+
+class StateViewSet(ModelViewSet):
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
+
+
+class SymptomsViewSet(ModelViewSet):
+    queryset = Symptoms.objects.all()
+    serializer_class = SymptomsSerializer
+
+
+class TelephoneViewSet(ModelViewSet):
+    queryset = Telephone.objects.all()
+    serializer_class = TelephoneSerializer
+
+
+class TestViewSet(ModelViewSet):
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
+
+
+class TestCategoryViewSet(ModelViewSet):
+    queryset = TestCategory.objects.all()
+    serializer_class = TestCategorySerializer
+
+
+class TestSubcategoryViewSet(ModelViewSet):
+    queryset = TestSubcategory.objects.all()
+    serializer_class = TestSubcategorySerializer
+
+
+class TestimonialsViewSet(ModelViewSet):
+    queryset = Testimonials.objects.all()
+    serializer_class = TestimonialsSerializer
+
+
+class TimeslotsViewSet(ModelViewSet):
+    queryset = Timeslots.objects.all()
+    serializer_class = TimeslotsSerializer
+
+
+class TrxnAppointmentbookingsViewSet(ModelViewSet):
+    queryset = TrxnAppointmentbookings.objects.all()
+    serializer_class = TrxnAppointmentbookingsSerializer
+
+
+class TrxnCouponsViewSet(ModelViewSet):
+    queryset = TrxnCoupons.objects.all()
+    serializer_class = TrxnCouponsSerializer
+
+
+class TrxnDiscountcardViewSet(ModelViewSet):
+    queryset = TrxnDiscountcard.objects.all()
+    serializer_class = TrxnDiscountcardSerializer
+
+
+class TrxnHealthcheckbookingViewSet(ModelViewSet):
+    queryset = TrxnHealthcheckbooking.objects.all()
+    serializer_class = TrxnHealthcheckbookingSerializer
+
+
+class TrxnPaymentsViewSet(ModelViewSet):
+    queryset = TrxnPayments.objects.all()
+    serializer_class = TrxnPaymentsSerializer
+
+
+class UsersViewSet(ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+
+class WeekViewSet(ModelViewSet):
+    queryset = Week.objects.all()
+    serializer_class = WeekSerializer
+
+
+class WellnessplanViewSet(ModelViewSet):
+    queryset = Wellnessplan.objects.all()
+    serializer_class = WellnessplanSerializer
+
+
+class WellnessplanProfessionViewSet(ModelViewSet):
+    queryset = WellnessplanProfession.objects.all()
+    serializer_class = WellnessplanProfessionSerializer
+
+
+class WellnessplanSocialhistoryViewSet(ModelViewSet):
+    queryset = WellnessplanSocialhistory.objects.all()
+    serializer_class = WellnessplanSocialhistorySerializer
