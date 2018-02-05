44d48bd970f122a38e43734fe7987dcaa83e1b51
sounak98 <sounak.98@gmail.com>
2018-02-05 21:11:35 +0530

    adds healthprovider dashboard templates

--- phealth/healthprovider/urls.py
+++ phealth/healthprovider/urls.py
@@ -5,8 +5,21 @@ from . import views
 
 app_name = "healthprovider"
 
+dashboard_routes = [
+	url(r'^$', views.Dashboard, name='dashboard'),
+	url(r'^basic_details/?$', views.BasicDetails, name='basic_details'),
+	url(r'^contact_details/?$', views.ContactDetails, name='contact_details'),
+	url(r'^branches/?$', views.Branches, name='branches'),
+	url(r'^specialities/?$', views.Specialities, name='specialities'),
+	url(r'^facilities/?$', views.Facilities, name='facilities'),
+	url(r'^offerings/?$', views.Offerings, name='offerings'),
+	url(r'^special_health_checks/?$', views.SpecialHealthChecks, name='special_health_checks'),
+	url(r'^plans/?$', views.Plans, name='plans'),
+]
+
 urlpatterns = [
     url(r'^signup/?$', views.SignUp, name='signup'),
-    url(r'^signin/?$', views.SignIn, name='signin')
+    url(r'^signin/?$', views.SignIn, name='signin'),
+    url(r'^dashboard/', include(dashboard_routes)),
 ]
 
--- phealth/healthprovider/views.py
+++ phealth/healthprovider/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from phealth.utils import match_role
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -26,6 +27,58 @@ def SignIn(request):
 		return redirect('healthprovider:signin')
 
 
-@match_role("healthprovider")
-def dashboard(request):
-	return JsonResponse({'status' : True })
+# @match_role("healthprovider")
+def Dashboard(request):
+	return render(request, 'healthprovider/dashboard/base.html.j2', context={
+		"title" : "Dashboard",
+		"user" : "User"
+		})
+
+def BasicDetails(request):
+	return render(request, 'healthprovider/dashboard/basic_details.html.j2', context={
+		"title" : "Basic Details",
+		"section" : "Basic Details",
+		})
+
+def ContactDetails(request):
+	return render(request, 'healthprovider/dashboard/contact_details.html.j2', context={
+		"title" : "Contact Details",
+		"section" : "Contact Details",
+		})
+
+def Branches(request):
+	return render(request, 'healthprovider/dashboard/branches.html.j2', context={
+		"title" : "Branches",
+		"section" : "Branches",
+		})
+
+def Specialities(request):
+	return render(request, 'healthprovider/dashboard/specialities.html.j2', context={
+		"title" : "Specialities",
+		"section" : "Specialities",
+		})
+
+
+def Facilities(request):
+	return render(request, 'healthprovider/dashboard/facilities.html.j2', context={
+		"title" : "Facilities",
+		"section" : "Facilities",
+		})
+
+def Offerings(request):
+	return render(request, 'healthprovider/dashboard/offerings.html.j2', context={
+		"title" : "Offerings",
+		"section" : "Offerings",
+		})
+
+def SpecialHealthChecks(request):
+	return render(request, 'healthprovider/dashboard/special_health_checks.html.j2', context={
+		"title" : "Special Health Checks",
+		"section" : "Special Health Checks",
+		})
+
+def Plans(request):
+	return render(request, 'healthprovider/dashboard/plans.html.j2', context={
+		"title" : "Plans",
+		"section" : "Plans",
+		})
