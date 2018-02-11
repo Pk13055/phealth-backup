e4af6d43112af884ad7eb931b807cde892ac9fad
sounak98 <sounak.98@gmail.com>
2018-02-11 20:49:13 +0530

    updates healthprovider dashboard views

--- phealth/healthprovider/views.py
+++ phealth/healthprovider/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from phealth.utils import match_role
+from phealth.utils import match_role, signin, redirect
 from django.http import JsonResponse
 from django import forms
-from api.models import Healthproviders
+from api.models import Healthproviders, Users, HealthprovidersSpeciality, Availablefacilities
 # Create your views here.
 
 
@@ -25,10 +25,10 @@ def SignIn(request):
 			})
 	elif request.method == "POST":
 		if signin("healthprovider", request):
-			return redirect('healthprovider:dashboard')
+			return redirect('healthprovider:dashboard_home')
 		return redirect('healthprovider:signin')
 
-@match_role("sponsor")
+@match_role("healthprovider")
 def dashboard(request):
 	''' route for dashboard home '''
 
@@ -52,45 +52,135 @@ def dashboard(request):
 		"form" : BasicForm(instance=u)
 	})
 
+@match_role("healthprovider")
 def contact_details(request):
-	return render(request, 'healthprovider/dashboard/contact_details.html.j2', context={
-		"title" : "Contact Details",
-		"section" : "Contact Details",
-		})
+	''' route for dashboard contact details '''
 
-def branches(request):
-	return render(request, 'healthprovider/dashboard/branches.html.j2', context={
-		"title" : "Branches",
-		"section" : "Branches",
-		})
+	u = Healthproviders.objects.filter(users__email=request.session['email']).first()
+
+	class BasicForm(forms.ModelForm):
+
+		class Meta:
+			model = Users
+			fields = ('mobile', 'email', 'firstname', 'middlename', 'lastname')
+
+	if request.method == "POST":
+		b = BasicForm(request.POST, request.FILES, instance=u.users)
+		if b.is_valid():
+			b.save()
+		print(b)
+
+	return render(request, 'healthprovider/dashboard/home.html.j2', context={
+		"title": "Contact Details Dashboard",
+		"form_title" : "Edit Contact Information",
+		"form" : BasicForm(instance=u.users)
+	})
+
+# def branches(request):
+# 	return render(request, 'healthprovider/dashboard/branches.html.j2', context={
+# 		"title" : "Branches",
+# 		"section" : "Branches",
+# 		})
 
 def specialities(request):
-	return render(request, 'healthprovider/dashboard/specialities.html.j2', context={
-		"title" : "Specialities",
-		"section" : "Specialities",
-		})
+	''' route for dashboard specialities '''
+
+	u = Healthproviders.objects.filter(users__email=request.session['email']).first()
+
+	class SpecialityForm(forms.ModelForm):
+		class Meta:
+			model = HealthprovidersSpeciality
+			fields = ('__all__')
+			widgets = {
+				'validity' : forms.TextInput(attrs={
+					'placeholder' : "YYYY-MM-DD"
+				})
+			}
+
+	EditFormSet = forms.modelformset_factory(HealthprovidersSpeciality, fields=('__all__'), extra=0)
+
+	if request.method == "POST":
+		_forms = []
+		if request.POST['data_type'] == "add":
+			c = SpecialityForm(request.POST, request.FILES)
+			_forms.append(c)
+		elif request.POST['data_type'] == "update":
+			c = EditFormSet(request.POST, request.FILES)
+			_forms += c.forms
+
+		for form in _forms:
+			if form.is_valid():
+				d = form.save(commit=False)
+				d.healthproviders = u
+				d.save()
+			else:
+				print("errors :", form.errors)
+
+
+	edit_forms = EditFormSet(queryset=HealthprovidersSpeciality.objects.filter(healthproviders__healthproviders_id=u.healthproviders_id))
+	return render(request, 'healthprovider/dashboard/speciality.html.j2', context={
+		"title": "Specialities Dashboard",
+		"form" : SpecialityForm(),
+		"edit_forms": edit_forms
+	})
 
 
 def facilities(request):
-	return render(request, 'healthprovider/dashboard/facilities.html.j2', context={
-		"title" : "Facilities",
-		"section" : "Facilities",
-		})
+	''' route for dashboard facilities '''
 
-def offerings(request):
-	return render(request, 'healthprovider/dashboard/offerings.html.j2', context={
-		"title" : "Offerings",
-		"section" : "Offerings",
-		})
+	u = Healthproviders.objects.filter(users__email=request.session['email']).first()
 
-def special_health_checks(request):
-	return render(request, 'healthprovider/dashboard/special_health_checks.html.j2', context={
-		"title" : "Special Health Checks",
-		"section" : "Special Health Checks",
-		})
+	class FacilityForm(forms.ModelForm):
+		class Meta:
+			model = Availablefacilities
+			fields = ('facilities_services', 'facilities')
+			widgets = {
+				'validity' : forms.TextInput(attrs={
+					'placeholder' : "YYYY-MM-DD"
+				})
+			}
 
-def plans(request):
-	return render(request, 'healthprovider/dashboard/plans.html.j2', context={
-		"title" : "Plans",
-		"section" : "Plans",
-		})
+	EditFormSet = forms.modelformset_factory(Availablefacilities, fields=('facilities_services', 'facilities'), extra=0)
+
+	if request.method == "POST":
+		_forms = []
+		if request.POST['data_type'] == "add":
+			c = FacilityForm(request.POST, request.FILES)
+			_forms.append(c)
+		elif request.POST['data_type'] == "update":
+			c = EditFormSet(request.POST, request.FILES)
+			_forms += c.forms
+
+		for form in _forms:
+			if form.is_valid():
+				d = form.save(commit=False)
+				d.healthproviders = u
+				d.save()
+			else:
+				print("errors :", form.errors)
+
+
+	edit_forms = EditFormSet(queryset=Availablefacilities.objects.filter(healthproviders__healthproviders_id=u.healthproviders_id))
+	return render(request, 'healthprovider/dashboard/facility.html.j2', context={
+		"title": "Facilities Dashboard",
+		"form" : FacilityForm(),
+		"edit_forms": edit_forms
+	})
+
+# def offerings(request):
+# 	return render(request, 'healthprovider/dashboard/offerings.html.j2', context={
+# 		"title" : "Offerings",
+# 		"section" : "Offerings",
+# 		})
+
+# def special_health_checks(request):
+# 	return render(request, 'healthprovider/dashboard/special_health_checks.html.j2', context={
+# 		"title" : "Special Health Checks",
+# 		"section" : "Special Health Checks",
+# 		})
+
+# def plans(request):
+# 	return render(request, 'healthprovider/dashboard/plans.html.j2', context={
+# 		"title" : "Plans",
+# 		"section" : "Plans",
+# 		})
