1df8beb2acde11b9ab096739ea32f9d35f806f39
sounak98 <sounak.98@gmail.com>
2018-02-13 23:32:36 +0530

    adds PayU biz integration

--- phealth/common/urls.py
+++ phealth/common/urls.py
@@ -5,6 +5,13 @@ from . import views
 
 app_name = "common"
 
+payment_routes = [
+	url(r'^$', views.payment_init, name='payment_init'),
+	url(r'^success/?$', views.payment_success, name='payment_success'),
+	url(r'^failure/?$', views.payment_failure, name='payment_failure'),
+]
+
 urlpatterns = [
 	url(r'^signout/?$', views.logout, name='signout'),
+	url(r'^payment/', include(payment_routes)),
 ]
--- phealth/common/views.py
+++ phealth/common/views.py
@@ -1,6 +1,14 @@
+import datetime
+import hashlib
+from random import randint
+
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.template.context_processors import csrf
+from phealth import utils
+from api.models import User
+
 # Create your views here.
 
 @csrf_exempt
@@ -16,5 +24,21 @@ def logout(request):
 def home_route(request):
 	return render(request, 'home.html.j2', context={
 		'title' : "HOME"
-		})
+	})
 
+# payment routes
+def payment_success(request):
+	return JsonResponse({"status": True})
+
+def payment_failure(request):
+	return JsonResponse({"status": False})
+
+def payment_init(request):
+	action, data = utils.generate_payment_data(request, 'user', 123.45, 'test')
+
+	if data:
+		return render(request, 'common/payment/init.html.j2', context={
+			'title': 'Payment',
+			'action': action,
+			'data': data
+		})
