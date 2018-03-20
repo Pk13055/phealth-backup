ca82b658712e600e5fa8cb54c357f7e2cecca3c0
pk13055 <pkrockstar7@gmail.com>
2018-03-21 00:57:42 +0530

    Healthprovider dashboard linked
    - HTML templates linked
    - Routes added
    - Static files linked

--- phealth/healthprovider/urls.py
+++ phealth/healthprovider/urls.py
@@ -5,14 +5,68 @@ from . import views
 
 app_name = "healthprovider"
 
+account_routes = [
+	url(r'^basic/?$', views.account_basic, name="account_basic"),
+	url(r'^contact/?$', views.account_contact, name="account_contact"),
+	url(r'^speciality/?$', views.account_speciality, name="account_speciality"),
+	url(r'^facilities/?$', views.account_facilities, name="account_facilities"),
+	url(r'^offerings/?$', views.account_offerings, name="account_offerings"),
+	url(r'^special_checks/?$', views.account_special_checks, name="account_special_checks"),
+]
+
+branch_update_routes = [
+	url(r'^basic/?$', views.branch_basic, name="branch_basic_update"),
+	url(r'^contact/?$', views.branch_contact, name="branch_contact_update"),
+	url(r'^facilities/?$', views.branch_facilities, name="branch_facilities_update"),
+	url(r'^healthcheck/?$', views.branch_healthcheck, name="branch_healthcheck_update"),
+	url(r'^offerings/?$', views.branch_offerings, name="branch_offerings_update"),
+	url(r'^organization/?$', views.branch_organization, name="branch_organization_update"),
+	url(r'^speciality/?$', views.branch_speciality, name="branch_speciality_update"),
+]
+
+branch_routes = [
+	url(r'^new/?$', views.branch_new, name="branch_new"),
+	url(r'^view/?$', views.branch_view, name="branch_view"),
+	url(r'^update/', include(branch_update_routes)),
+]
+
+appointment_routes = [
+	url(r'^daily/?$', views.appointment_daily, name="appointment_daily"),
+	url(r'^weekly/?$', views.appointment_weekly, name="appointment_weekly"),
+	url(r'^monthly/?$', views.appointment_monthly, name="appointment_monthly"),
+]
+
+payment_routes = [
+	url(r'^new/?$', views.payment_new, name="payment_new"),
+	url(r'^add/?$', views.payment_add, name="payment_add"),
+	url(r'^view/?$', views.payment_view, name="payment_view"),
+]
+
+clinician_routes = [
+	url(r'^new/?$', views.clinician_new, name="clinician_new"),
+	url(r'^view/?$', views.clinician_view, name="clinician_view"),
+]
+
+# dashboard routes
 dashboard_routes = [
+	
+	# old routes
 	url(r'^$', views.dashboard, name='dashboard_home'),
 	url(r'^branches/?$', views.branches, name='branches'),
 	url(r'^specialities/?$', views.specialities, name='specialities'),
 	url(r'^clinicians/?$', views.clinicians, name='clinicians'),
 	url(r'^appointments/?$', views.appointments, name='appointments'),
+
+	# new routes
+	url(r'^home/?$', views.dashboard_home, name='dashboard_home_new'),
+	url(r'^account/', include(account_routes)),
+	url(r'branches/', include(branch_routes)),
+	url(r'appointments/', include(appointment_routes)),
+	url(r'clinicians/', include(clinician_routes)),
+	url(r'payments/', include(payment_routes)),
 ]
 
+# actual routes
 urlpatterns = [
     url(r'^signup/?$', views.SignUp, name='signup'),
     url(r'^signin/?$', views.SignIn, name='signin'),
--- phealth/healthprovider/views.py
+++ phealth/healthprovider/views.py
@@ -1,12 +1,12 @@
 import random
 
-from django.shortcuts import render
-from phealth.utils import match_role, signin, redirect
-from django.http import JsonResponse
 from django import forms
 from django.contrib.auth.hashers import make_password
-from api.models import (User, Provider, Speciality,
-	Clinician, Appointment)
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from api.models import Appointment, Clinician, Provider, Speciality, User
+from phealth.utils import match_role, redirect, signin
 
 # Create your views here.
 
@@ -208,4 +208,310 @@ def appointments(request):
 	return render(request, 'healthprovider/dashboard/appointment.html.j2', context={
 		"title": "Appointment List",
 		"appointments": appointments.items(),
+	})
+
+# NEW ROUTES
+
+# @match_role("healthprovider")
+def dashboard_home(request):
+	''' route for dashboard home  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/index.html.j2', context={
+		'title' : "Dashboard - Home",
+	})
+
+# account routes
+
+
+# @match_role("healthprovider")
+def account_basic(request):
+	''' route for account - basic  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/account/basic.html.j2', context={
+		'title' : "account - Basic",
+	})
+
+
+# @match_role("healthprovider")
+def account_contact(request):
+	''' route for account - contact  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/account/contact.html.j2', context={
+		'title' : "account - contact",
+	})
+
+
+# @match_role("healthprovider")
+def account_speciality(request):
+	''' route for account - speciality  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/account/speciality.html.j2', context={
+		'title' : "account - speciality",
+	})
+
+
+# @match_role("healthprovider")
+def account_facilities(request):
+	''' route for account - facilities  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/account/facilities.html.j2', context={
+		'title' : "account - facilities",
+	})
+
+
+# @match_role("healthprovider")
+def account_offerings(request):
+	''' route for account - offerings  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/account/offerings.html.j2', context={
+		'title' : "account - offerings",
+	})
+
+
+# @match_role("healthprovider")
+def account_special_checks(request):
+	''' route for account - special_checks  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/account/special_checks.html.j2', context={
+		'title' : "account - special_checks",
+	})
+
+
+# Branch Routes
+
+
+# @match_role("healthprovider")
+def branch_new(request):
+	''' route for branch - new  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/branch/new.html.j2', context={
+		'title' : "branch - new",
+	})
+
+
+# @match_role("healthprovider")
+def branch_view(request):
+	''' route for branch - view  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/branch/view.html.j2', context={
+		'title' : "branch - view",
+	})
+
+	
+# branch update routes
+
+
+# @match_role("healthprovider")
+def branch_basic(request):
+	''' route for branch update - branch_basic'''
+
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+	
+
+	return render(request, 'healthprovider/dashboard/branch/branch_basic.html.j2', context={
+		'title' : "Update - branch_basic"
+	})
+
+
+# @match_role("healthprovider")
+def branch_contact(request):
+	''' route for branch update - branch_contact'''
+
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+	
+
+	return render(request, 'healthprovider/dashboard/branch/branch_contact.html.j2', context={
+		'title' : "Update - branch_contact"
+	})
+
+
+# @match_role("healthprovider")
+def branch_facilities(request):
+	''' route for branch update - branch_facilities'''
+
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+	
+
+	return render(request, 'healthprovider/dashboard/branch/branch_facilities.html.j2', context={
+		'title' : "Update - branch_facilities"
+	})
+
+
+# @match_role("healthprovider")
+def branch_healthcheck(request):
+	''' route for branch update - branch_healthcheck'''
+
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+	
+
+	return render(request, 'healthprovider/dashboard/branch/branch_healthcheck.html.j2', context={
+		'title' : "Update - branch_healthcheck"
+	})
+
+
+# @match_role("healthprovider")
+def branch_offerings(request):
+	''' route for branch update - branch_offerings'''
+
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+	
+
+	return render(request, 'healthprovider/dashboard/branch/branch_offerings.html.j2', context={
+		'title' : "Update - branch_offerings"
+	})
+
+
+# @match_role("healthprovider")
+def branch_organization(request):
+	''' route for branch update - branch_organization'''
+
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+	
+
+	return render(request, 'healthprovider/dashboard/branch/branch_organization.html.j2', context={
+		'title' : "Update - branch_organization"
+	})
+
+
+# @match_role("healthprovider")
+def branch_speciality(request):
+	''' route for branch update - branch_speciality'''
+
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+	
+
+	return render(request, 'healthprovider/dashboard/branch/branch_speciality.html.j2', context={
+		'title' : "Update - branch_speciality"
+	})
+
+
+
+# Appointment Routes
+
+
+# @match_role("healthprovider")
+def appointment_daily(request):
+	''' route for appointment - daily  '''
+
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+	
+	return render(request, 'healthprovider/dashboard/appointment/daily.html.j2', context={
+		'title' : "appointment - daily",
+	})
+
+
+# @match_role("healthprovider")
+def appointment_weekly(request):
+	''' route for appointment - weekly  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/appointment/weekly.html.j2', context={
+		'title' : "appointment - weekly",
+	})
+
+
+# @match_role("healthprovider")
+def appointment_monthly(request):
+	''' route for appointment - monthly  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/appointment/monthly.html.j2', context={
+		'title' : "appointment - monthly",
+	})
+
+
+# Payment Routes
+
+
+# @match_role("healthprovider")
+def payment_new(request):
+	''' route for payment - new  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/payment/new.html.j2', context={
+		'title' : "payment - new",
+	})
+
+
+# @match_role("healthprovider")
+def payment_add(request):
+	''' route for payment - add  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/payment/add.html.j2', context={
+		'title' : "payment - add",
+	})
+
+
+# @match_role("healthprovider")
+def payment_view(request):
+	''' route for payment - view  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/payment/view.html.j2', context={
+		'title' : "payment - view",
+	})
+
+
+# Clinician Routes
+
+
+# @match_role("healthprovider")
+def clinician_new(request):
+	''' route for clinician - new  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/clinician/new.html.j2', context={
+		'title' : "clinician - new",
+	})
+
+	
+# @match_role("healthprovider")
+def clinician_view(request):
+	''' route for clinician - view  '''
+	
+	p = Provider.objects.filter(poc__email=request.session['email']).first()
+
+
+	return render(request, 'healthprovider/dashboard/clinician/view.html.j2', context={
+		'title' : "clinician - view",
 	})
