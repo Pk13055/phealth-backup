5b818bf607b15bd20950e21d9f932b62ba720a93
unknown <rakesh@mbrinformatics.com>
2018-04-03 17:07:15 +0530

    speciality, test category , test subcategory crud functionality implemented

--- phealth/site_admin/forms.py
+++ phealth/site_admin/forms.py
@@ -14,3 +14,41 @@ class SymptomsForm(forms.ModelForm):
         for myField in self.fields:
             self.fields[myField].widget.attrs['class'] = 'form-control'
             self.fields[myField].widget.attrs['placeholder'] = myField
+
+class SpecialityForm(forms.ModelForm):
+
+    class Meta:
+        model = Speciality
+        fields = ('name', 'description',)
+
+
+    def __init__(self, *args, **kwargs):
+        super(SpecialityForm, self).__init__(*args, **kwargs)
+        for myField in self.fields:
+            self.fields[myField].widget.attrs['class'] = 'form-control'
+            self.fields[myField].widget.attrs['placeholder'] = myField
+class TestcategoryForm(forms.ModelForm):
+
+    class Meta:
+        model = TestCategory
+        fields = ('name', 'description',)
+
+
+    def __init__(self, *args, **kwargs):
+        super(TestcategoryForm, self).__init__(*args, **kwargs)
+        for myField in self.fields:
+            self.fields[myField].widget.attrs['class'] = 'form-control'
+            self.fields[myField].widget.attrs['placeholder'] = myField
+
+class TestsubcategoryForm(forms.ModelForm):
+
+    class Meta:
+        model = TestSubcategory
+        fields = ('name', 'category','description',)
+
+
+    def __init__(self, *args, **kwargs):
+        super(TestsubcategoryForm, self).__init__(*args, **kwargs)
+        for myField in self.fields:
+            self.fields[myField].widget.attrs['class'] = 'form-control'
+            self.fields[myField].widget.attrs['placeholder'] = myField
